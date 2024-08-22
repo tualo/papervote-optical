334787f74f183582564896a1c34f330b89c782af
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-08-22 14:30:50 +0200

    add missed files

---
+++ src/sql/install/stimmzettel_roi.sql
@@ -0,0 +1,9 @@
+DELIMITER ;
+CREATE TABLE IF NOT EXISTS `stimmzettel_roi` (
+  `stimmzettel_id` int(11) NOT NULL,
+  `sz_rois_id` int(11) NOT NULL,
+  PRIMARY KEY (`stimmzettel_id`,`sz_rois_id`),
+  KEY `fk_stimmzettel_roi_sz_rois` (`sz_rois_id`),
+  CONSTRAINT `fk_stimmzettel_roi_stimmzettel` FOREIGN KEY (`stimmzettel_id`) REFERENCES `stimmzettel` (`id`),
+  CONSTRAINT `fk_stimmzettel_roi_sz_rois` FOREIGN KEY (`sz_rois_id`) REFERENCES `sz_rois` (`id`)
+) ;
