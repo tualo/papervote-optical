e1be66950ca96eda5ef659edcef1c3f5f4546fd3
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2024-10-11 11:55:11 +0200

    fix error

--- src/js/both/PaginationRenderer.js
+++ src/js/both/PaginationRenderer.js
@@ -2,25 +2,27 @@
 Ext.define('Tualo.PaperVoteOptical.Format', {
     singleton: true,
     paginationRenderer:  function (value, metaData, record) {
+        try{
+            if ((record.get('stimmzettelgruppen_enthaltung') == 1) || (record.get('stimmzettelgruppen_enthaltung') === true)) {
+                metaData.tdStyle = 'background-color: rgba(255,255,0,0.5);;';
+            }
 
-        if ((record.get('stimmzettelgruppen_enthaltung') == 1) || (record.get('stimmzettelgruppen_enthaltung') === true)) {
-            metaData.tdStyle = 'background-color: rgba(255,255,0,0.5);;';
+            if ((record.get('stimmzettelgruppen_ungueltig') == 1) || (record.get('stimmzettelgruppen_ungueltig') === true)) {
+                metaData.tdStyle = 'background-color: rgba(255,50,0,0.5);';
+            }
+
+            if ((record.get('stimmzettel_enthaltung') == 1) || (record.get('stimmzettel_enthaltung') === true)) {
+                metaData.tdStyle = 'background-color: rgba(255,255,0,0.5);;';
+            }
+
+
+            if ((record.get('stimmzettel_ungueltig') == 1) || (record.get('stimmzettel_ungueltig') === true)) {
+                metaData.tdStyle = 'background-color: rgba(255,0,0,0.5);';
+            }
+
+        }catch(e){
+            console.error(e);
         }
-
-        if ((record.get('stimmzettelgruppen_ungueltig') == 1) || (record.get('stimmzettelgruppen_ungueltig') === true)) {
-            metaData.tdStyle = 'background-color: rgba(255,50,0,0.5);';
-        }
-
-        if ((record.get('stimmzettel_enthaltung') == 1) || (record.get('stimmzettel_enthaltung') === true)) {
-            metaData.tdStyle = 'background-color: rgba(255,255,0,0.5);;';
-        }
-
-
-        if ((record.get('stimmzettel_ungueltig') == 1) || (record.get('stimmzettel_ungueltig') === true)) {
-            metaData.tdStyle = 'background-color: rgba(255,0,0,0.5);';
-        }
-
-
 
 
         return value;
@@ -29,7 +31,7 @@ Ext.define('Tualo.PaperVoteOptical.Format', {
         if (value==1 || value==true ){
             return '<i class="fa-solid fa-square-check"></i>';
         }
-        return '<i class="fa-solid fa-square"></i>';
+        return '<i class="fa-regular fa-square"></i>';
 
     }
 });
