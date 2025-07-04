4c31d3184a410ed3fac4b6e39a1bb2d8e3cc479d
Steffen Borchmann <steffen.borchmann@tualo.de>
2025-07-04 10:34:15 +0200

    add security helper for static file serving

--- src/Routes/JSLoader.php
+++ src/Routes/JSLoader.php
@@ -4,18 +4,17 @@ namespace Tualo\Office\PaperVoteOptical\Routes;
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
-
+use Tualo\Office\Basic\RouteSecurityHelper;
 class JsLoader implements IRoute{
     public static function register(){
         BasicRoute::add('/jspapervoteoptical/(?P<file>[\w.\/\-]+).js',function($matches){
-            App::contenttype('application/javascript');
-            if (file_exists(dirname(__DIR__,1).'/js/lazy/'.$matches['file'].'.js')){
-                App::etagFile( dirname(__DIR__,1).'/js/lazy/'.$matches['file'].'.js', true);
-                BasicRoute::$finished = true;
-                http_response_code(200);
-            } 
-            
-            
+            RouteSecurityHelper::serveSecureStaticFile(
+                $matches['file'] . '.js',
+                dirname(__DIR__, 1) . '/js/lazy/',
+                ['js'],
+                ['application/javascript']
+            );            
+
         },['get'],false);
 
     }
