d8bff1d853b571ec8a438650ff2cce08860f3581
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-02-17 10:25:47 +0100

    Version bump to 2.1.4

--- src/sql/install/sz_titel_regions.sql
+++ src/sql/install/sz_titel_regions.sql
@@ -13,13 +13,5 @@ CREATE TABLE IF NOT EXISTS `sz_titel_regions` (
 
 
 
-create or replace view view_readtable_sz_titel_regions as
-select 
-    sz_titel_regions.*,
-    sz_to_region.id_sz
-from 
-    sz_titel_regions 
-    left join  sz_to_region
-        on sz_to_region.id_sz_titel_regions = sz_titel_regions.id
-;
+
 
--- src/sql/install/sz_to_region.sql
+++ src/sql/install/sz_to_region.sql
@@ -7,3 +7,14 @@ CREATE TABLE IF NOT EXISTS `sz_to_region` (
   CONSTRAINT `fk_id_stimmzettel` FOREIGN KEY (`id_sz`) REFERENCES `stimmzettel` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_id_sz_titel_regions` FOREIGN KEY (`id_sz_titel_regions`) REFERENCES `sz_titel_regions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ;
+
+
+create or replace view view_readtable_sz_titel_regions as
+select 
+    sz_titel_regions.*,
+    sz_to_region.id_sz
+from 
+    sz_titel_regions 
+    left join  sz_to_region
+        on sz_to_region.id_sz_titel_regions = sz_titel_regions.id
+;
