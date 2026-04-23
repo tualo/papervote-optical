22cb2071ff0285e45cf77ebb2792473903888dd3
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-04-23 11:38:41 +0200

    switch proc to new table struc

--- src/sql/install/proc_papervote_optical.sql
+++ src/sql/install/proc_papervote_optical.sql
@@ -49,91 +49,106 @@ BEGIN
         set use_box_id = if( ifnull( r_box_id,0) ='',0, ifnull(r_box_id,0) );
         set use_stack_id = if( ifnull(r_stack_id,0) ='',0, ifnull(r_stack_id,0) );
 
+        if (substring(use_box_id,1,3) = 'FC8') then
+            insert ignore into kisten1 (
+                id,
+                login,
+                createdatetime,
+                update_time
+            ) values (
+                use_box_id,
+                r_login,
+                cast(r_created as datetime),
+                cast(r_modified as time)
+            );
 
-        insert ignore into kisten2 (
-            ridx,
-            id,
-            kostenstelle,
-            name,
-            aktiv,
-            insert_date,
-            update_date,
-            login,
-            insert_time,
-            update_time
-        ) values (
-            use_box_id,
-            regexp_replace(use_box_id,'[^0-9]',''),
-            0,
-            use_box_id,
-            1,
-            cast(r_created as date),
-            cast(r_modified as date),
-            getSessionUser(),
-            cast(r_created as time),
-            cast(r_modified as time)
-        );
-        insert ignore into stapel2 (
-            ridx,
-            id,
-            kostenstelle,
-            name,
-            kisten2,
-            aktiv,
-            insert_date,
-            update_date,
-            login,
-            insert_time,
-            update_time
-        ) values (
-            use_stack_id,
-            regexp_replace(use_stack_id,'[^0-9]',''),
-            0,
-            use_stack_id,
-            use_box_id,
-            1,
-            cast(r_created as date),
-            cast(r_modified as date),
-            getSessionUser(),
-            cast(r_created as time),
-            cast(r_modified as time)
-        );
+            insert ignore into stapel1 (
+                id,
+                login,
+                kisten1,
+                abgebrochen,
+                createdatetime
+                ) values (
+                use_stack_id,
+                r_login,
+                use_box_id,
+                0,
+                cast(r_created as datetime)
+            );
 
+            insert ignore into stimmzettel1 (
+                id,
+                login,
+                stapel1,
+                abgebrochen,
+                createdatetime,
+                stimmzettel
+            ) values (
+                r_pagination_id,
+                r_login,
+                use_stack_id,
+                0,
+                cast(r_created as datetime),
+                r_ballotpaper_id
+            );
+        end if;
+
+
+        if (substring(use_box_id,1,3) = 'FC4') then
+            insert ignore into kisten2 (
+                id,
+                login,
+                createdatetime,
+                update_time
+            ) values (
+                use_box_id,
+                r_login,
+                cast(r_created as datetime),
+                cast(r_modified as time)
+            );
+
+            insert ignore into stapel2 (
+                id,
+                login,
+                kisten2,
+                abgebrochen,
+                createdatetime
+                ) values (
+                use_stack_id,
+                r_login,
+                use_box_id,
+                0,
+                cast(r_created as datetime)
+            );
+
+            insert ignore into stimmzettel2 (
+                id,
+                login,
+                stapel2,
+                abgebrochen,
+                createdatetime,
+                stimmzettel
+            ) values (
+                r_pagination_id,
+                r_login,
+                use_stack_id,
+                0,
+                cast(r_created as datetime),
+                r_ballotpaper_id
+            );
+        end if;
         
+        if (substring(use_box_id,1,3) = 'FC8') then
+            delete from kandidaten1 where stimmzettel1 = r_pagination_id ;
+        end if;
 
-
-        insert into stimmzettel2 (
-            ridx,
-            id,
-            kostenstelle,
-            name,
-            stapel2,
-            aktiv,
-            insert_date,
-            update_date,
-            login,
-            insert_time,
-            update_time,
-            stimmzettel
-        ) values (
-            r_pagination_id,
-            r_pagination_id,
-            0,
-            r_pagination_id,
-            use_stack_id,
-            1,
-            cast(r_created as date),
-            cast(r_modified as date),
-            getSessionUser(),
-            cast(r_created as time),
-            cast(r_modified as time),
-            concat(r_ballotpaper_id,'|0')
-        );
-
+        if (substring(use_box_id,1,3) = 'FC4') then
+            delete from kandidaten2 where stimmzettel2 = r_pagination_id ;
+        end if;
+        
         for can in (
             select 
                 view_papervote_optical_result_ballotpaper_trigger.* ,
-                kandidaten.ridx,
                 kandidaten.barcode
             from 
                 view_papervote_optical_result_ballotpaper_trigger 
@@ -142,42 +157,38 @@ BEGIN
             ) 
         do
 
+            if (substring(use_box_id,1,3) = 'FC8') then
+                insert into kandidaten1(
+                    login,
+                    kandidaten,
+                    stimmzettel1,
+                    createdatetime,
+                    stimmen
+                ) values (
+                    r_login,
+                    can.kandidaten_id,
+                    r_pagination_id,
+                    cast(r_created as datetime),
+                    1
+                ) ;
+            end if;
 
-            insert into kandidaten2(
-                ridx,
-                id,
-                kostenstelle,
-                name,
-                stimmzettel2,
-                kandidaten,
-
-
-                aktiv,
-                insert_date,
-                update_date,
-                login,
-                insert_time,
-                update_time
-            ) values (
-                concat(lpad(r_pagination_id,12,'0') , lpad(can.result_index,3,'0')),
-                r_pagination_id,
-                can.kandidaten_id,
-                can.barcode,
-                r_pagination_id,
-                can.ridx,
-
-
-                1,
-                cast(r_created as date),
-                cast(r_modified as date),
-                getSessionUser(),
-                cast(r_created as time),
-                cast(r_modified as time)
-
-
-            ) ; 
+            if (substring(use_box_id,1,3) = 'FC4') then
+                insert into kandidaten2(
+                    login,
+                    kandidaten,
+                    stimmzettel2,
+                    createdatetime,
+                    stimmen
+                ) values (
+                    r_login,
+                    can.kandidaten_id,
+                    r_pagination_id,
+                    cast(r_created as datetime),
+                    1
+                ) ;
+            end if;
         end for; 
-
 
     end if;
 
